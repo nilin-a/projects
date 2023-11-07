eb25464408ef902266f1dfedbe069dce20d4dafd
nilin-a <liza.ry-samara@mail.ru>
2023-11-07 20:56:18 +0400

    tried to do rowset

--- online_job_portal/src/com/company/Retrieving.java
+++ online_job_portal/src/com/company/Retrieving.java
@@ -1,4 +1,8 @@
 package com.company;
+import javax.sql.rowset.JdbcRowSet;
+import javax.sql.rowset.RowSetProvider;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.sql.* ;
 
 public class Retrieving {
@@ -16,5 +20,26 @@ public class Retrieving {
         }
         connection.close();
         return  resultSet;
+    }
+
+    public static void retrieveWithRowSetFromTable(String query) {
+        try {
+            URL myURL = new URL("jdbc:postgresql://localhost:5432/online_job_portal_db");
+            JdbcRowSet rowSet = RowSetProvider.newFactory().createJdbcRowSet();
+            rowSet.setURL(1, myURL);
+            rowSet.setUsername("postgres");
+            rowSet.setPassword("123456");
+            rowSet.setCommand(query);
+            rowSet.execute();
+            while (rowSet.next()) {
+                System.out.println(rowSet.getObject(1));
+                System.out.println(rowSet.getObject(2));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
