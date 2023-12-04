a779e65136f1b6718645095b945c0d88212bafb1
nilin-a <liza.ry-samara@mail.ru>
2023-12-04 16:58:37 +0400

    smth was added

--- sb_practice/src/main/java/com/example/sb_practice/controllers/CourseController.java
+++ sb_practice/src/main/java/com/example/sb_practice/controllers/CourseController.java
@@ -1,15 +1,19 @@
 package com.example.sb_practice.controllers;
 
 import com.example.sb_practice.entities.Course;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class CourseController {
-    @RequestMapping("/courses")
+    @GetMapping("/courses")
     public List<Course> retrieveAllCourses() {
         return Arrays.asList(
                 new Course(1, "java", "liza"),
@@ -18,4 +22,24 @@ public class CourseController {
         );
     }
 
+    @GetMapping("hi-html")
+    public String greetingHtml() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("<html>");
+        stringBuffer.append("<head>");
+        stringBuffer.append("<title>");
+        stringBuffer.append("Title hi");
+        stringBuffer.append("</title>");
+        stringBuffer.append("</head>");
+        stringBuffer.append("<body>");
+        stringBuffer.append("Body hi");
+        stringBuffer.append("</body>");
+        stringBuffer.append("/<html>");
+        return stringBuffer.toString();
+    }
+    @GetMapping("/hi")
+    public ModelAndView helloJSP() {
+        int a =0;
+        return new ModelAndView("hello");
+    }
 }
